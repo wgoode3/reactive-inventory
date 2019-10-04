7ad009f7803d8a2931617984b66e3a5c65ea0530
William Goode <wgoode3@gmail.com>
2019-10-04 14:43:29 -0400

    inventory app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function sort(arr, key) {
+  for(let i=0; i<arr.length; i++) {
+    for(let j=0; j<arr.length-i-1; j++) {
+      if(arr[j][key] < arr[j+1][key]) {
+        let tmp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = tmp;
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inventory : [
+        {product: 'monitors', count: 0},
+        {product: 'mice', count: 0},
+        {product: 'chairs', count: 0},
+        {product: 'hdmi-dvi connnectors', count: 0},
+        {product: 'mini-dp connectors', count: 0}
+      ]
+    }
+  }
+  increment(i, amt) {
+    let temp = [...this.state.inventory];
+    temp[i].count += amt;
+    sort(temp, 'count');
+    this.setState({inventory: temp});
+
+  }
+  render() {
+    return (
+      <>
+        <h1>Inventory</h1>
+        <table border="1">
+          <tbody>
+          <tr>
+            <th>Product Name</th>
+            <th>Quantity</th>
+            <th>Adjust</th>
+          </tr>
+          {
+            this.state.inventory.map( (item, i) => 
+              <tr key={i}>
+                <td>{item.product}</td>
+                <td>{item.count}</td>
+                <td>
+                  <button onClick={this.increment.bind(this, i, 1)}>+</button>
+                  <button onClick={this.increment.bind(this, i, -1)}>-</button>
+                </td>
+              </tr>
+            )
+          }
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
 
 export default App;
